fd218c7d235a7c001582bd181bae26a9ded3ff02
mujm4tic <mmoosavi@hotmail.co.uk>
2022-04-11 17:53:30 +0100

    dto methods added and tests started

--- src/test/java/com/sparta/sc/Injector.java
+++ src/test/java/com/sparta/sc/Injector.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.sc.DTO.WeatherDTO;
 
 import java.io.IOException;
+import java.net.http.HttpResponse;
 
 public class Injector {
-    public WeatherDTO injectDTO(String url){
+    public static WeatherDTO injectDTO(HttpResponse<String> response){
         WeatherDTO weatherDTO = new WeatherDTO();
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            weatherDTO = objectMapper.readValue(ConnectionManager.getConnectionResponse().body(), WeatherDTO.class);
+            weatherDTO = objectMapper.readValue(response.body(), WeatherDTO.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
