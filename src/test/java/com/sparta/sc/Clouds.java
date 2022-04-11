4ab4dcecf9508511282a817bcec599069ebd3018
mujm4tic <mmoosavi@hotmail.co.uk>
2022-04-11 13:59:34 +0100

    dto created

---
+++ src/test/java/com/sparta/sc/Clouds.java
@@ -0,0 +1,13 @@
+package com.sparta.sc;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Clouds{
+
+	@JsonProperty("all")
+	private int all;
+
+	public int getAll(){
+		return all;
+	}
+}
