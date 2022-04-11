622fc2468a6625ab7753edd18e27b143f60df824
sams4566 <sams4566@gmail.com>
2022-04-11 14:00:52 +0100

    Add properties loader

--- src/test/java/com/sparta/sc/ConnectionManager.java
+++ src/test/java/com/sparta/sc/ConnectionManager.java
@@ -7,7 +7,8 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConnectionManager {
-    private static final String BASEURL = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=97c6edb288230b9ed8dcbd4680220d4f";
+    private static String key = PropertiesLoader.getProperties().getProperty("key");
+    private static String BASEURL = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=" + key;
 
     public static String getConnection() {
         getResponse();
@@ -25,7 +26,6 @@ public class ConnectionManager {
         }
         return httpResponse;
     }
-
     public static HttpResponse<String> getConnectionResponse() {
         return getResponse();
     }
