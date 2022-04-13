811d165fe9e8d20f1bee8cdfaf1eafd82adccf49
Simone Conigliaro <sconigliaro@spartaglobal.com>
2022-04-13 12:27:00 +0100

    fixed errors

--- src/test/java/com/sparta/sc/WeatherDTOByLonLatTests.java
+++ src/test/java/com/sparta/sc/WeatherDTOByLonLatTests.java
@@ -1,13 +1,9 @@
 package com.sparta.sc;
 
 import com.sparta.sc.dto.WeatherDTO;
-import com.sparta.sc.utilities.SpeedConverter;
-import com.sparta.sc.utilities.TemperatureConverter;
 import org.junit.jupiter.api.*;
 
 import static com.sparta.sc.ConnectionManager.getConnection;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WeatherDTOByLonLatTests {
 
